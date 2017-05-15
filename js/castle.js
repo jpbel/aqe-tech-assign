71ab6cef220f71a24b7a836524b8957b2200b58c
jpbel <jpbelzile@softdrivecorp.com>
2017-05-15 12:25:50 -0700

    changed buildCastles to output number of castles

--- js/castle.js
+++ js/castle.js
@@ -2,9 +2,11 @@ function buildCastles(landArr){
 	var lastCastle;
 	var lastIdx;
 	var castleArr = [];
+	var numCastles = 0;
 	for(var i = 0; i < landArr.length; i++){
 		if(i === 0){
 			castleArr.push("C");
+			numCastles++;
 		}else if(landArr[i] > landArr[i - 1]){
 			markCastle("C^", i);
 		}else if(landArr[i] < landArr[i - 1]){
@@ -19,16 +21,18 @@ function buildCastles(landArr){
 		wasFalseCastle(type);
 		lastIdx = i;
 		lastCastle = type;
+		numCastles++;
 	}
 
 	function wasFalseCastle(type){
 		if(lastCastle === type){
 			castleArr[lastIdx] =  null;
+			numCastles--;
 		}
 	}
 	
 
-	return castleArr;
+	return numCastles;
 
 
 	
